4d1ff2730a548e7c454391209b84a2ff614fa8bb
Aaron Solenberg <amsolenberg@gmail.com>
2025-03-29 11:51:33 -0400

    module 3 assignment 3 start (files copied)

---
+++ Module 3/03 - Star Wars/starWars/App.js
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Platform } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Home from './Home';
+import Planets from './Planets';
+import Films from './Films';
+import Spaceships from './Spaceships';
+
+const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+function renderScreens(Navigator) {
+  return (
+    <Navigator.Navigator>
+      <Navigator.Screen
+        name='Home'
+        component={Home}
+      />
+      <Navigator.Screen
+        name='Planets'
+        component={Planets}
+      />
+      <Navigator.Screen
+        name='Films'
+        component={Films}
+      />
+      <Navigator.Screen
+        name='Spaceships'
+        component={Spaceships}
+      />
+    </Navigator.Navigator>
+  );
+}
+
+export default function App() {
+  const isIOS = Platform.OS === 'ios';
+
+  return <NavigationContainer>{isIOS ? renderScreens(Tab) : renderScreens(Drawer)}</NavigationContainer>;
+}
